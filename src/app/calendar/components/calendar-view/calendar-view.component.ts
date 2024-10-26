e837b2fb6729e8537b29dab9db9d54b7b7336ad6
M.Bagheri <MahdiBagheriVar@gmail.com>
2024-10-26 20:26:47 +0330

    feat: Add drag-and-drop functionality to calendar view

--- src/app/calendar/components/calendar-view/calendar-view.component.ts
+++ src/app/calendar/components/calendar-view/calendar-view.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-calendar-view',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './calendar-view.component.html',
   styleUrl: './calendar-view.component.scss'
 })
-export class CalendarViewComponent {
+export class CalendarViewComponent implements OnInit {
+  appointments = [
+    { title: 'Meeting', date: new Date(), time: '10:00' },
+    { title: 'Dentist', date: new Date(), time: '14:00' }
+  ];
 
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.appointments, event.previousIndex, event.currentIndex);
+  }
 }
